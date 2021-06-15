7d634566def48a5c3c279fb5c13491095a59ef69
Manuel Duarte <carlosd99m@gmail.com>
2021-06-15 16:18:46 -0600

    creando todas las capetas y los archivos

--- crud_nodejs/app.js
+++ crud_nodejs/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Motor de plantilla
+app.set('view engine', 'ejs')
+
+// Llamanos a las rutas
+
+app.use('/', require('./routes/router'))
 
 app.listen(9999, ()=>{
     console.log('Server corriendo en el puerto 9999')
