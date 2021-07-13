aae0adf69bb2ab8d30f53d055c5c39cfe6c85a6b
manuelduarte077 <carlosd99m@gmail.com>
2021-07-13 12:43:29 -0600

    Configuración de servidor web parte 2

--- control-gastos/node/index.js
+++ control-gastos/node/index.js
@@ -1,11 +1,15 @@
 
 const express = require('express')
+const movimientoRouter = require('./routes/movimientos')
 const app = express()
 
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json)
 
+app.use('movimientos', movimientoRouter)
+
+
 app.listen(3000, ()=>{
-    console.log('Corriendo en el puerto 300')
+    console.log('Corriendo en el puerto 3000')
 })
