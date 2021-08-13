62d81f97353b100e5a2945f5ab8071c999a8f908
manuelduarte077 <carlosd99m@gmail.com>
2021-08-13 00:44:35 -0600

    update

--- crud-students-golang/main.go
+++ crud-students-golang/main.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"net/http"
 	"text/template"
-	//_ "github.com/go-sql-driver/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type employee struct {
--- learn_golang/02-valores.go
+++ learn_golang/02-valores.go
@@ -19,4 +19,6 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+
+
 }
