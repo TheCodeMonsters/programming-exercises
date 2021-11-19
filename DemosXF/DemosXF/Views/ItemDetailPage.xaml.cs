c7d0317b0985ee960c683aff033965cf8e6133ca
Manuel Duarte <carlosd99m@gmail.com>
2021-11-19 00:04:53 -0600

    remove folder

---
+++ DemosXF/DemosXF/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DemosXF.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DemosXF.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
