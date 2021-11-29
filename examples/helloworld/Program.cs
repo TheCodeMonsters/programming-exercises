cf3cbc19835e9629f88448d3bec34b805f0613a1
Manuel Duarte <carlosd99m@gmail.com>
2021-11-29 02:29:51 -0600

    feat: add ejemplos con c#

---
+++ examples/helloworld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string firstName;
+            string lastName;
+            int age;
+
+            // Inicializacion de las variables
+            firstName = "Manuel";
+            lastName = "Duarte";
+            age = 20;
+
+            // Imprimir en consola
+            Console.Write("Hello, ");
+            Console.Write( firstName );
+            Console.Write( " " );
+            Console.Write( lastName );
+            Console.Write(" Tu tienes una edad de: ");
+            System.Console.Write(age);
+
+
+            // se asegura que el programa no se cerrará inmediatamente después 
+            // de ejecutarse
+            System.Console.ReadKey();
+
+        }
+    }
+}
