23f1deab9da251f2f181045f83c601d21120864e
Manuel Duarte <carlosd99m@gmail.com>
2021-12-13 01:13:59 -0600

    dibujando un escudo en c#

---
+++ examples/dibujoTriangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        // dibujo de cada linea (bucle externo)
+        for (int fila = 1; fila <= 7; fila++)
+        {
+            // dibuja espacios en blanco (1er bucle interno)
+            for (int espacios = 7 - fila; espacios > 0; espacios--)
+                Console.Write(" "); // Espacio en blanco
+
+            // Dibuja estrellas (3do bucle interno)
+            for (int conta = 1; conta < (2 * fila); conta++)
+                Console.Write("*");
+
+            Console.WriteLine(" ");
+        }
+    }
+}
