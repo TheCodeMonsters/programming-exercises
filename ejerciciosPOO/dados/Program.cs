d7b26fdeeb1640272847274b99b2f607145d58a4
Manuel Duarte <carlosd99m@gmail.com>
2021-12-23 23:39:30 -0600

    ejercicio de los dados

--- ejerciciosPOO/dados/Program.cs
+++ ejerciciosPOO/dados/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Dados
+{
+    class Dado
+    {
+        private int valor;
+        private static Random aleatorio;
+
+        public Dado()
+        {
+            aleatorio = new Random();
+        }
+
+        public void Tirar()
+        {
+            valor = aleatorio.Next(1, 7);
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El valor del dado es: " + valor);
+        }
+
+        public int RetornarValor()
+        {
+            return valor;
+        }
+
+    }
+
+    class JuegoDados
+    {
+        private Dado dado1, dado2, dado3;
+
+        public JuegoDados()
+        {
+            dado1 = new Dado();
+            dado2 = new Dado();
+            dado3 = new Dado();
+        }
+
+        public void Jugar()
+        {
+            dado1.Tirar();
+            dado1.Imprimir();
+
+            dado2.Tirar();
+            dado2.Imprimir();
+
+            dado3.Tirar();
+            dado3.Imprimir();
+
+            if (dado1.RetornarValor() == dado2.RetornarValor() &&
+                dado1.RetornarValor() == dado3.RetornarValor())
+            {
+                Console.WriteLine("Ganaste");
+            }
+            else
+            {
+                Console.WriteLine("Perdiste");
+            }
+            Console.ReadKey();
+
+        }
+
+        static void Main(string[] args)
+        {
+            JuegoDados juego = new JuegoDados();
+            juego.Jugar();
+        }
+
+    }
+
+}
---
+++ ejerciciosPOO/dados/README.md
@@ -0,0 +1,22 @@
+
+## Plantear un programa que permita jugar a los dados. Las reglas de juego son: se tiran tres dados si los tres salen con el mismo valor mostrar un mensaje que "gano", sino "perdió".\
+
+
+### Lo primero que hacemos es identificar las clases:
+### Luego los atributos y los métodos de cada clase:
+
+- Dado  
+    - atributos
+        - valor
+    - métodos
+        - constructor
+        - Tirar
+        - Imprimir
+        - RetornarValor
+
+- JuegoDeDados
+    - atributos
+        - 3 Dado (3 objetos de la clase Dado)
+    - métodos
+        - constructor
+        - Jugar
